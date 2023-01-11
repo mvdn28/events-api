8e179f24ba3ae002ba53be4b0300153b59884974
mvdn28 <vieira.mauricio94@gmail.com>
2023-01-11 15:26:49 -0300

    first commit

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const db = require("./src/database");
 
 db.hasConnection();
 
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}...`);
